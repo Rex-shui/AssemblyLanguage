cf51afe2a6fb2f79986deab3dd5dfc82007e4f7a
shui <33775186+Rex-shui@users.noreply.github.com>
2020-07-01 12:29:11 +0800

    Create anyJmp.asm

---
+++ notes/anyJmp.asm
@@ -0,0 +1,31 @@
+assume cs:code
+code segment
+
+	  mov ax,4c00h
+	  int 21h
+	
+start:
+	  mov ax,0
+	
+s:	nop
+	  nop
+	
+	  mov di,offset s
+	  mov si,offset s2
+	  mov ax,cs:[si]
+	  mov cs:[di],ax  ;将标号s处的内容变为E8F6
+	
+s0:	jmp short s
+
+s1: mov ax,0
+	  int 21h
+	  mov ax,0
+	
+s2: jmp short s1    ;E8F6
+	  nop
+
+
+	
+code ends 
+
+end start
