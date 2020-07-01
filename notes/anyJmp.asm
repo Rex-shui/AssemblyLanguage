775822054661d25b81c8dbac0223d874061056ae
shui <33775186+Rex-shui@users.noreply.github.com>
2020-07-01 12:30:09 +0800

    Update anyJmp.asm

--- notes/anyJmp.asm
+++ notes/anyJmp.asm
@@ -7,7 +7,7 @@ code segment
 start:
 	  mov ax,0
 	
-s:	nop
+s:	  nop
 	  nop
 	
 	  mov di,offset s
@@ -15,13 +15,13 @@ s:	nop
 	  mov ax,cs:[si]
 	  mov cs:[di],ax  ;将标号s处的内容变为E8F6
 	
-s0:	jmp short s
+s0:	  jmp short s
 
-s1: mov ax,0
+s1: 	  mov ax,0
 	  int 21h
 	  mov ax,0
 	
-s2: jmp short s1    ;E8F6
+s2: 	  jmp short s1    ;E8F6
 	  nop
 
 
